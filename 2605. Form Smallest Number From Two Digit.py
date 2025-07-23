255ff4315d93e6ca399d719aa1fddb29aeea0636
Rishabh Singh Chauhan <143814425+Rsc2414@users.noreply.github.com>
2025-07-23 23:03:58 +0530

    Create 2605. Form Smallest Number From Two Digit.py

---
+++ 2605. Form Smallest Number From Two Digit.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minNumber(self, nums1: List[int], nums2: List[int]) -> int:
+        ans = float('inf')
+        
+        for i in nums1:
+            for j in nums2:
+                if i == j:
+                    a = i
+                else:
+                    a = i * 10
+                    a += j
+                    b = j * 10
+                    b += i
+                    a = min(a,b)
+                ans = min(ans,a)
+        
+        return ans
+
+        
